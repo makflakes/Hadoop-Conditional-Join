1eb13fc5c93c8a4ce70eefb7a02bcda258d5ad9a
Abdul Khaliq <31276822+makflakes@users.noreply.github.com>
2021-05-02 11:24:38 +0300

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+
 import sys
 
 for line in sys.stdin:
     line       = line.strip()   
     key_value  = line.split(",")   
-    key_in     = key_value[0]   
-    value_in   = key_value[1]    
+    show     = key_value[0]   
+    channel   = key_value[1]   
 
-    #print key_in
-    if (value_in.isdigit()):					
-    	print( '%s\t%s' % (key_in, value_in) )  
-    elif (value_in == "ABC"):
-    	print( '%s\t%s' % (key_in, value_in) )  
+    #print show
+    if (channel.isdigit()):					
+    	print( '%s\t%s' % (show, channel) )  
+    elif (channel == "ABC"):
+    	print( '%s\t%s' % (show, channel) )
 
