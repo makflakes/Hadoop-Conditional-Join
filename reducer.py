bc55b0a59dbfc806576ac9270ab8d6e011f8c642
Abdul Khaliq <31276822+makflakes@users.noreply.github.com>
2021-05-02 11:23:59 +0300

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -2,7 +2,7 @@
 import sys
 
 prev_show	= None		
-total_views = 0			
+total_channel = 0			
 print_flag  = 0 		
 
 for line in sys.stdin:
@@ -11,22 +11,21 @@ for line in sys.stdin:
 
      
     curr_show  = key_value[0]         
-    value_in   = key_value[1]         
+    channel   = key_value[1]         
 
     if (prev_show != curr_show):
         if (print_flag == 1):
-            print('%s\t%s' % (prev_show, total_views))
+            print('%s\t%s' % (prev_show, total_channel))
             print_flag = 0
-            total_views = 0
-        if (value_in != "ABC"):					
-            (total_views) = int(value_in)		
+            total_channel = 0
+        if (channel != "ABC"):					
+            (total_channel) = int(channel)		
     else:										
-        if (value_in != "ABC"):					
-            (total_views) = int(total_views)	
-            (value_in)    = int(value_in)		
-            (total_views) = (total_views) + (value_in)	
-    if (value_in == "ABC"):
+        if (channel != "ABC"):					
+            (total_channel) = int(total_channel)	
+            (channel)    = int(channel)		
+            (total_channel) = (total_channel) + (channel)	
+    if (channel == "ABC"):
         print_flag = 1
     prev_show = curr_show	
-
 
